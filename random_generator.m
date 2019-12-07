c620e3fab874dc945d407815dc621330534a1469
Tolulope <tolesy2003@outlook.com>
2019-12-07 12:59:37 +0200

    Random patterns generator
    This script generate 32bit random numbers for different patterns

---
+++ random_generator.m
@@ -0,0 +1,17 @@
+function random_generator(bit_length, pattern_threshold,folder)
+patterns = random_number_test_lengths(pattern_threshold);
+for i = 1:length(patterns)
+    pattern = patterns(i);
+    for c = 1:pattern
+        for r = 1:bit_length
+            random_numbers = generate_random_number(bit_length);
+        end
+        file_pattern_number = sprintf('%d', pattern);
+        filepath = fullfile(folder, [file_pattern_number, '_data', '.txt']);
+        fileID = fopen(filepath,'a');
+        fprintf(fileID,'%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d \n',random_numbers);
+        fclose(fileID);
+    end
+end
+end
+
--- run_random_generator.m
+++ run_random_generator.m
@@ -1,17 +1,5 @@
 bit_length = 32;
 folder = fullfile ('../','gen');
 dir = mkdir(folder);
-patterns = random_number_test_lengths(150);
-for i = 1:length(patterns)
-    pattern = patterns(i);
-    for c = 1:pattern
-        for r = 1:bit_length
-            numbers = generate_random_number(bit_length);
-        end
-        file = sprintf('%d', pattern);
-        filepath = fullfile(folder, [file, '_data', '.txt']);
-        fileID = fopen(filepath,'a');
-        fprintf(fileID,'%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d \n',numbers);
-        fclose(fileID);
-    end
-end
+pattern_threshold = 4050;
+random_generator(bit_length, pattern_threshold, folder);
