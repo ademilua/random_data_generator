f6767e1e1be574179f959ac1d4b0abc4eee29297
Tolulope <tolesy2003@outlook.com>
2019-12-07 14:17:53 +0200

    Update run_random_generator.m

--- run_random_generator.m
+++ run_random_generator.m
@@ -1,5 +1,9 @@
-bit_length = 32;
+prompt = 'bit length:';
+prompt_2 = 'pattern length:';
+bit = input(prompt);
+pattern = input(prompt_2);
+bit_length = bit;
 folder = fullfile ('../','gen');
 dir = mkdir(folder);
-pattern_threshold = 4050;
+pattern_threshold = pattern;
 random_generator(bit_length, pattern_threshold, folder);
